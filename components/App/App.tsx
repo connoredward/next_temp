f966f563491760eeebc04faa5acec6a1669e6fab
connor <connor.chaplain@pipemind.com>
2020-07-12 00:07:53 +0100

    added tailwind

--- components/App/App.tsx
+++ components/App/App.tsx
@@ -1,3 +1,10 @@
-const App: React.FunctionComponent = () => <div>Hello</div>;
+import styles from './styles.scss';
+
+const App: React.FunctionComponent = () => (
+  <div className={styles.test}>
+    <h1 className={styles.header}>Next.js</h1>
+    <p className={styles.sub}>with Tailwind CSS</p>
+  </div>
+);
 
 export default App;
